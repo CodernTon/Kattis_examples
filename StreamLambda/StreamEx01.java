66f8d06ad83bb68868cf3005d1728d2430f6acfa
Anton Lyngfelt <anton.lyngfelt@hotmail.com>
2019-05-17 14:29:00 +0200

    more changes to streams

--- StreamLambda/StreamEx01.java
+++ StreamLambda/StreamEx01.java
@@ -9,28 +9,41 @@ public class StreamEx01{
     List<Person> people = Arrays.asList(
       new Person("Anton", "Lyngfelt",23),
       new Person("Erik", "Oscarsson",45),
-      new Person("Carl","Davidsson",15)
+      new Person("Carl","Davidsson",15),
+      new Person("Alex","Johansson",32)
       );
 
     System.out.println("\nSort reverse by age:");
     Comparator<Person> compAge = (aName, bName) ->
         aName.getAge().compareTo(bName.getAge());
-    
-    Comparator<Person> compFirstName = (aName, bName) ->
+
+    Comparator<Person> compFirstNameRev = (aName, bName) ->
         aName.getFirstName().compareTo(bName.getFirstName());
-    
-    
+
+    Comparator<Person> compFirstName = (aName, bName) ->
+        bName.getFirstName().compareTo(aName.getFirstName());
+
+
     //Make a stream, sort by age
     people.stream()
         .sorted(compAge.reversed())
         .forEach(System.out::println);
-    
+
     System.out.println("\nSort reverse by first name:");
     //Make a stream, sort by name
     people.stream()
+        .sorted(compFirstNameRev.reversed())
+        .forEach(System.out::println);
+
+    System.out.println("\nPrint out standard list: ");
+    people.stream()
+        .forEach(System.out::println);
+
+    System.out.println("\nPrint out natural order list: ");
+    people.stream()
         .sorted(compFirstName.reversed())
         .forEach(System.out::println);
-    
+
     System.out.println("WAAAH");
   }
 }
